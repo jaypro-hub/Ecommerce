ac801bc441d57f6fa9c727f4f9d5388ec934de38
vipul <vipulrp0445@gamail.com>
2019-08-16 19:04:25 +0530

    First Commited for project

---
+++ Ecommerce.Repo/Interface/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using Ecommerce.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Repo
+{
+   
+        public interface IRepository<T> where T : BaseEntity
+        {
+            IEnumerable<T> GetAll();
+            T Get(long id);
+            void Insert(T entity);
+            void Update(T entity);
+            void Delete(T entity);
+            void Remove(T entity);
+            void SaveChanges();
+        }
+}
---
+++ Ecommerce.Service/Interface/IUserService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using Ecommerce.Data;
+
+namespace Ecommerce.Service
+{
+   public interface IUserService
+    {
+        IEnumerable<User> GetUsers();
+        User GetUser(long id);
+        void InsertUser(User user);
+        void UpdateUser(User user);
+        void DeleteUser(long id);
+    }
+}

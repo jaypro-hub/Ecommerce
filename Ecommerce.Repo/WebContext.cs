60daa02241f66dcf2aa3220281f36d05a82ff1de
vipul <vipulrp0445@gamail.com>
2019-08-17 15:58:54 +0530

    Intial Puss

---
+++ Ecommerce.Repo/WebContext.cs
@@ -0,0 +1,19 @@
+﻿using Ecommerce.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Repo
+{
+    public class WebContext : DbContext
+    {
+        public WebContext()
+            : base("DefaultConnection")
+        {
+        }
+        public DbSet<TemporyTest> TemporyTests { get; set; }
+    }
+}
